014394cfa6d2e5e225f657bb0234ee5ac1e612f1
Alekossta <alekosstavrinoudakis@gmail.com>
2024-09-23 09:09:05 +0300

    show window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,52 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include <iostream>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define APPLICATION_NAME "OpenGL_Template"
+
+void window_resized_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
 
 int main()
 {
+	glfwInit();
 
+	// set opengl to 3.3 version core profile
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create the window
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, APPLICATION_NAME, NULL, NULL);
+	if (!window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// init GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// bind our window resize event. will be called always once on start
+	glfwSetFramebufferSizeCallback(window, window_resized_callback);
+
+	// render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
